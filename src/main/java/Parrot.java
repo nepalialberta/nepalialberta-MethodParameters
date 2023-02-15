e594fdfbba3e00ad44a10a10ff079237522a5497
nepalialberta <nepalialberta@gmail.com>
2023-02-15 19:17:02 +0000

    Done

--- src/main/java/Parrot.java
+++ src/main/java/Parrot.java
@@ -26,6 +26,16 @@ public class Parrot {
      * @return text.
      */
     public String copy(String text){
-        return "return text please!";
+
+       
+        return text;
+
+        }
+
+        public static void main(String[] args){
+
+            Parrot newText = new Parrot();
+            newText.copy("Hello");
+
+        }
     }
-}
